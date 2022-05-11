92e13fe9d200f93741016f5cef05771192cd2f3d
Gustavo Henrique <ghenriquer10@gmail.com>
2022-05-11 18:45:50 -0300

    Criando página de filmes curtidos

--- src/pages/LikedMovies/index.js
+++ src/pages/LikedMovies/index.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import * as C from './style';
+import emptyMovie from '../../assets/video-camera-vazio.png'
 
 export default function LikedMovies(){
+    const [loading, setLoading] = useState(true)
+    const [likedMovies, setLikedMovies] = useState()
+
+    useEffect(() => {
+        const likedMoviesStorage = localStorage.getItem('likedMovies')
+        const likedMovies = JSON.parse(likedMoviesStorage)
+        setLikedMovies(likedMovies)
+        console.log(likedMovies)
+        setLoading(false)
+    }, [])
+
     return(
         <C.Container>
-            <h1>Filmes curtidos</h1>
+                <div className='title'>
+                    <h1>Filmes curtidos</h1>
+                </div>
+                {loading && 
+                    <div className='loading'>
+                        <h1>Carregando filmes curtidos...</h1>
+                    </div>
+                }
+            
+                {!likedMovies ?
+                    <C.EmptyMovie>
+                        <div className='noMovie'>
+                            <img src={emptyMovie} alt="Imagem de uma câmera representando a ausência de filmes"/>
+                            <h1>Nenhum filme</h1>
+                        </div>  
+                    </C.EmptyMovie>:
+                    <C.HasMovies>
+                        {likedMovies.map((movie) => {
+                            return(
+                                <C.Card>
+                                    
+                                </C.Card>
+                            )
+                        })}
+                    </C.HasMovies>
+                }
         </C.Container>
     )
 }
--- src/pages/LikedMovies/style.js
+++ src/pages/LikedMovies/style.js
@@ -1,9 +1,53 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    height: 100%;
     width: 100%;
+    height: auto;
+    background-color: #fff;
+    font-family: sans-serif;
+    font-size: 0.9em;
+    text-transform: uppercase;
 
-    background-color: black;
-    color: white;
+    .loading{
+        border: 1px solid red;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100px;
+        width: 100%;
+    }
+
+    .title{
+        display: flex;
+        align-items: center;
+        margin: 50px 0 0 200px;
+        height: 100px;
+    }
+`;
+
+export const EmptyMovie = styled.div`
+    .noMovie{
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+
+        h1{
+            margin: 50px 0;
+        }
+    }
+`;
+
+export const HasMovies = styled.div`
+    display: grid;
+    grid-gap: 100px;
+    grid-template-columns: repeat(3, 1fr);   
+`;
+
+export const Card = styled.div`
+    margin: auto;
+    border: 1px solid red;
+    height: 350px;
+    width: 320px;
 `;
